a34ecad911283452338333962e28eff613e5ec8f
Pota <turnernathan270@gmail.com>
2025-04-17 16:42:17 +1000

    Added service to enable/disable camera_vision_node

--- camera_vision_ros2wrap/camera_vision_ros2wrap/camera_vision_node.py
+++ camera_vision_ros2wrap/camera_vision_ros2wrap/camera_vision_node.py
@@ -2,6 +2,7 @@ import rclpy
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import PointCloud2
 from eufs_msgs.msg import BoundingBoxes
+from std_srvs.srv import SetBool
 
 
 class CameraVisionServer(rclpy.Node):
@@ -9,6 +10,11 @@ class CameraVisionServer(rclpy.Node):
     def __init__(self):
         super().__init__('/camera_vision_server')
 
+        # Parameters
+        self.running = True
+        publish_period = 0.5  # seconds
+
+        # Subscriptions
         self.sub_img_left = self.create_subscription(
             Image,
             '/zed/zed_node/left_raw/image_raw_color',
@@ -24,7 +30,7 @@ class CameraVisionServer(rclpy.Node):
         )
         self.img_right_buffer: Image = None
 
-        publish_period = 0.5  # seconds
+        # Timer and Publishers
         self.timer = self.create_timer(publish_period, self.pub_data_callback)
         self.pub_filtered_pointcloud = self.create_publisher(
             PointCloud2, 
@@ -36,6 +42,8 @@ class CameraVisionServer(rclpy.Node):
             '/utsma_vision/bounding_boxes', 
             10
         )
+
+        self.srv = self.create_service(SetBool, '/utsma_vision/enabled', self.enable_service_callback)
         
     def left_img_zed_callback(self, msg: Image):
         self.img_left_buffer = msg
@@ -43,8 +51,11 @@ class CameraVisionServer(rclpy.Node):
         self.img_right_buffer = msg
 
     def pub_data_callback(self):
+        if (self.running == False):
+            return
         if ((self.img_left_buffer == None) or (self.img_right_buffer == None)):
             return
+        
         
         left_img = self.img_left_buffer
         right_img = self.img_right_buffer
@@ -57,6 +68,21 @@ class CameraVisionServer(rclpy.Node):
 
         self.pub_filtered_pointcloud.publish(pc_msg)
         self.pub_bounding_boxes.publish(bounding_boxes_msg)
+
+    def enable_service_callback(self, request: SetBool.Request, response: SetBool.Response):
+        if (request.data == True):
+            self.running = True
+            response.success = True
+            response.message = "Successfully enabled camera_vision_server"
+        elif (request.data == False):
+            self.running = False
+            response.success = True
+            response.message = "Successfully disabled camera_vision_server"
+        else:
+            response.success = False
+            response.message = "request.data is not a bool"
+
+        return response
 
 
 
